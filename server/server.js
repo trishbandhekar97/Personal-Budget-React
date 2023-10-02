e832ebbabb6a9d83d94166b1fff8b2e44ed0ae60
Trish <trishbandhekar97@outlook.com>
2023-10-01 21:39:34 -0400

    Added server

---
+++ server/server.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+const port = 3000;
+
+app.use(cors());
+
+
+app.use('/', express.static('public'));
+
+
+app.get('/hello', (req,res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req,res)=>{
+    fs.readFile("data.json", "utf-8", (err,jsonData) => {
+
+        if (err) {
+            console.log("Error reading file:", err);
+            return;
+        }
+
+        try {
+            const budgetData = JSON.parse(jsonData);
+            res.json(budgetData);
+
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+})
+
+app.listen(port, ()=> {
+    console.log(`App running on port: ${port}`);
+});
